4f7dccbd9d67d920b73717c6b6a3b7b29db348c2
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-27 08:07:28 +0700

    repeat(value)
    repeat(value)

---
+++ Tugas19_2.js
@@ -0,0 +1,13 @@
+function Tugas19_2() {
+    var bintang = "*****";
+    console.log(bintang.repeat(1));
+    console.log(bintang.repeat(5));
+    console.log(bintang.repeat(10));
+    console.log(bintang.repeat(15));
+    console.log(bintang.repeat(20));
+    console.log(bintang.repeat(15));
+    console.log(bintang.repeat(10));
+    console.log(bintang.repeat(5));
+    console.log(bintang.repeat(1));
+}
+Tugas19_2();
